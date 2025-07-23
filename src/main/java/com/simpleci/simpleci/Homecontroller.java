e471b14fd48b1b501ba8572a5ccf63c051a1ea41
LimaSherzad <limasherzad55@gmail.com>
2025-07-23 13:59:50 +0430

    add

--- src/main/java/com/simpleci/simpleci/Homecontroller.java
+++ src/main/java/com/simpleci/simpleci/Homecontroller.java
@@ -1,16 +1,19 @@
 package com.simpleci.simpleci;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Homecontroller {
-//    git init
-//    git add README.md
-//    git commit -m "first commit"
-//    git branch -M main
-//    git remote add origin https://github.com/LimaShirzad/simpleDocker.git
-//    git push -u origin main
+
+
+
+    @GetMapping("/hello")
+    public  String sayHello()
+    {
+           return "Hello form docker spring boot appliction";
+    }
 
 
 }
